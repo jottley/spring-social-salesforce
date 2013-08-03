397f6e1409af8c25a4ca1b9d09fc6c18a0cfe9a2
Jared Ottley <jottley@gmail.com>
2013-08-02 18:16:44 -0600

    Revert "Revert "Add sObject Delete method. Method takes two parameters: The sObject type""
    This reverts commit f48019d63a1b90309cc70165fef80da8d1ecffd5.

--- src/main/java/org/springframework/social/salesforce/api/impl/SObjectsTemplate.java
+++ src/main/java/org/springframework/social/salesforce/api/impl/SObjectsTemplate.java
@@ -107,5 +107,12 @@ public class SObjectsTemplate extends AbstractSalesForceOperations<Salesforce> i
     }
     
     
+    @Override
+    public void delete(String sObjectName, String sObjectId)
+    {
+        requireAuthorization();
+        restTemplate.delete(api.getBaseUrl() + "/{version}/sobjects/{sObjectName}/{sObjectId}", API_VERSION, sObjectName, sObjectId);
+    }
+    
 
 }
